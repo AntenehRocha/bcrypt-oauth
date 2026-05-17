8f5978a8aca860abf58c0d51e7e07a1b352675bb
Anteneh Rocha <antenehrochaaotero@gmail.com>
2026-05-17 12:35:55 +0200

    feat: configure passport local strategy and serialization

--- index.js
+++ index.js
@@ -23,6 +23,34 @@ app.use(passport.session());
 // Mock User Database
 const users = [];
 
+// Passport Configuration
+passport.use(new LocalStrategy(async (username, password, done) => {
+    try {
+        const user = users.find(u => u.username === username);
+        if (!user) {
+            return done(null, false, { message: 'Incorrect username.' });
+        }
+
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) {
+            return done(null, false, { message: 'Incorrect password.' });
+        }
+
+        return done(null, user);
+    } catch (err) {
+        return done(err);
+    }
+}));
+
+passport.serializeUser((user, done) => {
+    done(null, user.username);
+});
+
+passport.deserializeUser((username, done) => {
+    const user = users.find(u => u.username === username);
+    done(null, user);
+});
+
 app.get('/', (req, res) => {
     res.send('<h1>Home</h1><p>Please login</p>');
 });
